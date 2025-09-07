cf7d23eb9c2d1b271240e2bc00b314839ad866d7
Chloe <cgreenstreet01@gmail.com>
2025-09-07 12:42:07 +0100

    07.09.25 working basic logic. to be extracted out and tested now. needs validation clauses after tests

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt" //means format, similar to Java’s System.out and Scanner
+)
+
+func main() {
+	pound := "£" //shorthand for declaring and initializing a variable
+	var hourlyRate, itemPrice float64
+	//^ this is so strange u can declare 2 variables on one line. float64 is javas double
+
+	fmt.Print("Enter your hourly rate: " + pound + "\n")
+	fmt.Scan(&hourlyRate)
+
+	fmt.Print("Enter the item's price:" + pound + "\n")
+	fmt.Scan(&itemPrice) //I think & is used to modify the variable here.
+
+	hours := itemPrice / hourlyRate
+	fmt.Printf("You need to work %.2f hours to afford this item.\n", hours)
+	//%.2f formats the float to 2 decimal places
+}
