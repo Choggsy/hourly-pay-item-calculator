d71a4bec21297074b6a2b3b380b7b9f122c58834
Chloe <cgreenstreet01@gmail.com>
2025-09-07 14:53:31 +0100

    07.09.25 calculator tests

--- utils/calculator_test.go
+++ utils/calculator_test.go
@@ -14,3 +14,26 @@ func Test_Calculates(t *testing.T) {
 }
 
 // to run an individual test its - go test -run TestName
+func Test_HalfHourRate(t *testing.T) {
+	expected := 2.0
+	result := CalculateWorkHours(25.0, 50.0)
+	if result != expected {
+		t.Errorf("Test failed, expected: %v, got: %v", expected, result)
+	}
+}
+
+func Test_ZeroHourlyRate(t *testing.T) {
+	expected := 0.0
+	result := CalculateWorkHours(25.0, 0.0)
+	if result != expected {
+		t.Errorf("Test failed, expected: %v, got: %v", expected, result)
+	}
+}
+
+func Test_PenceValue(t *testing.T) {
+	expected := 5.0
+	result := CalculateWorkHours(15.5, 77.5)
+	if result != expected {
+		t.Errorf("Test failed, expected: %v, got: %v", expected, result)
+	}
+}
