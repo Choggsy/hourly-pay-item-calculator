f2c98b248ce15d78b1e90d9d13fa75d45b46eeeb
Chloe <cgreenstreet01@gmail.com>
2025-09-07 13:06:19 +0100

    07.09.25 working extraction of calculator logic but the IDE hates the import sorting GOPATH or caches for this error

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt" //means format, similar to Java’s System.out and Scanner
+	"fmt"                                                 //means format, similar to Java’s System.out and Scanner
+	"github.com/Choggsy/hourly-pay-item-calculator/utils" //ide hates this import
 )
 
 func main() {
@@ -15,7 +16,7 @@ func main() {
 	fmt.Print("Enter the item's price:" + pound + "\n")
 	fmt.Scan(&itemPrice) //I think & is used to modify the variable here.
 
-	hours := itemPrice / hourlyRate
+	hours := utils.CalculateWorkHours(hourlyRate, itemPrice)
 	fmt.Printf("You need to work %.2f hours to afford this item.\n", hours)
 	//%.2f formats the float to 2 decimal places
 }
