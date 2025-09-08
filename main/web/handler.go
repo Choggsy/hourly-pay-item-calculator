1999665dd28edf9ee2e5d481d513c03200a580d6
Chloe <cgreenstreet01@gmail.com>
2025-09-08 21:39:06 +0100

    08.09.25 minor refactor before web server logic

--- main/web/handler.go
+++ main/web/handler.go
@@ -9,7 +9,12 @@ import (
 
 // WebHandler HTTP handlers for form processing
 func WebHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles(filepath.Join("main", "web", "templates", "form.html")))
+	tmpl := template.Must(template.ParseFiles(filepath.Join(
+		"main",
+		"web",
+		"templates",
+		"form.html",
+	)))
 
 	if r.Method == http.MethodGet {
 		tmpl.Execute(w, nil)
@@ -17,17 +22,12 @@ func WebHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	r.ParseForm()
-	rawRate := r.FormValue("hourlyRate")
-	rawPrice := r.FormValue("itemPrice")
-
-	hourlyRate, err1 := utils.ValidateAndParseFloat(rawRate)
-	itemPrice, err2 := utils.ValidateAndParseFloat(rawPrice)
+	hourlyRate, err1 := utils.ValidateAndParseFloat(r.FormValue("hourlyRate"))
+	itemPrice, err2 := utils.ValidateAndParseFloat(r.FormValue("itemPrice"))
 
 	if err1 != nil || err2 != nil {
 		tmpl.Execute(w, map[string]string{"Error": "Invalid input"})
 		return
 	}
-
-	hours := itemPrice / hourlyRate
-	tmpl.Execute(w, map[string]float64{"Hours": hours})
+	tmpl.Execute(w, map[string]float64{"Hours": itemPrice / hourlyRate})
 }
