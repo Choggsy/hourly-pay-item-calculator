58901616589161a279bb050d974160b229cf7c33
Chloe <cgreenstreet01@gmail.com>
2025-09-08 15:33:46 +0100

    08.09.25 suggested handler logic. need to refactor, test and compare logic for deeper understanding on private/public go packages and interactions with APIS/http

--- main/web/handler.go
+++ main/web/handler.go
@@ -1,6 +1,33 @@
 package web
 
+import (
+	"hourly-pay-item-calculator/utils"
+	"html/template"
+	"net/http"
+	"path/filepath"
+)
+
 // WebHandler HTTP handlers for form processing
-func WebHandler() string {
-	return "Web Handler"
+func WebHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles(filepath.Join("main", "web", "templates", "form.html")))
+
+	if r.Method == http.MethodGet {
+		tmpl.Execute(w, nil)
+		return
+	}
+
+	r.ParseForm()
+	rawRate := r.FormValue("hourlyRate")
+	rawPrice := r.FormValue("itemPrice")
+
+	hourlyRate, err1 := utils.ValidateAndParseFloat(rawRate)
+	itemPrice, err2 := utils.ValidateAndParseFloat(rawPrice)
+
+	if err1 != nil || err2 != nil {
+		tmpl.Execute(w, map[string]string{"Error": "Invalid input"})
+		return
+	}
+
+	hours := itemPrice / hourlyRate
+	tmpl.Execute(w, map[string]float64{"Hours": hours})
 }
