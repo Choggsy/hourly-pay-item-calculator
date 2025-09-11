4c3fff5db012bd85960d64d700c5d9174b2808fa
Chloe <cgreenstreet01@gmail.com>
2025-09-11 09:40:22 +0100

    11.09.25 remove redundentcys and fixed additional intelliJ cache imports

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,13 +11,8 @@ const server = ":8080"
 // main Web server entry point
 func main() {
 	http.HandleFunc("/", handler.WebHandler)
-	http.ListenAndServe(server, nil)
 
-	ErrorHandler()
 	log.Println("Starting server on " + server + "...")
-}
-
-func ErrorHandler() {
 	err := http.ListenAndServe(server, nil)
 	if err != nil {
 		log.Fatalf("Server failed: %v", err)
